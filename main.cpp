237e78cc9188437de256b6b86549388326586a7e
Ferrari404 <ricards.abols@icloud.com>
2026-09-13 18:33:12 +0300

    docs: pievienoti komentari koda

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// Funkcija kas izvada visus braucējus
 void printAll(string* names, int* points, int n) {
     for (int i = 0; i < n; i++)
         cout << names[i] << " - " << points[i] << " pts" << "\n";
 }
 
+// Funkcija kas atgriež braucēju ar visvairāk punktiem
 int findMaxIndex(int* points, int n) {
     int maxIndex = 0;
     for (int i = 1; i < n; i++)
@@ -13,6 +15,7 @@ int findMaxIndex(int* points, int n) {
     return maxIndex;
 }
 
+// Funkcija kas atgriež braucēju ar vismazāk punktiem
 int findMinIndex(int* points, int n) {
     int minIndex = 0;
     for (int i = 1; i < n; i++)
@@ -20,6 +23,7 @@ int findMinIndex(int* points, int n) {
     return minIndex;
 }
 
+// Funkcija kas aprēķina vidējo punktu skaitu
 double average(int* points, int n) {
     int sum = 0;
     for (int i = 0; i < n; i++) sum += points[i];
@@ -28,14 +32,16 @@ double average(int* points, int n) {
 
 int main() {
     int n = 4;
-    string* names = new string[n];
-    int* points = new int[n];
+    string* names = new string[n]; // Braucēju vārdu arrays
+    int* points = new int[n]; // Punktu arrays
 
+    // Ievada vārdu un punktus katram braucējam
     for (int i = 0; i < n; i++) {
         cout << "Ievadi pilota vardu: " << "\n";
         cin >> names[i];
         cout << "Ievadi punktus: " << "\n";
         cin >> points[i];
+        // Ja punkti nav derīgs skaitlis, liks ievadīt vēlreiz
         while (!cin.good()) {
             cin.clear();
             cin.ignore(256, '\n');
@@ -44,6 +50,7 @@ int main() {
         }
     }
 
+    // Izvade
     printAll(names, points, n);
     cout << names[findMaxIndex(points, n)] << " ir lideris" << endl;
     cout << names[findMinIndex(points, n)] << " Ir pedejais" << endl;
