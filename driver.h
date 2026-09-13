b57b88f58bb93b20700bb1ac59131b2e7cb2758f
Ferrari404 <ricards.abols@icloud.com>
2026-09-13 17:51:23 +0300

    feat: pievienota Driver struktra

---
+++ driver.h
@@ -0,0 +1,14 @@
+#ifndef DRIVER_H
+#define DRIVER_H
+
+#include <string>
+using namespace std;
+
+struct Driver {
+    string name;
+    string team;
+    int points;
+    double bestLapTime;
+};
+
+#endif
