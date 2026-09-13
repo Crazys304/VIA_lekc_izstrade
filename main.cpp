9d263bc8f835278e0c8ff8380794445c00bcc805
Ferrari404 <ricards.abols@icloud.com>
2026-09-13 18:12:00 +0300

    feat: pievienota printAll funkcija

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+void printAll(string* names, int* points, int n) {
+    for (int i = 0; i < n; i++)
+        cout << names[i] << " - " << points[i] << " pts" << endl;
+}
+
 int main() {
     int n = 3;
     string* names = new string[n];
@@ -9,6 +14,8 @@ int main() {
     names[0] = "Verstappen"; points[0] = 350;
     names[1] = "Norris";     points[1] = 320;
     names[2] = "Leclerc";    points[2] = 290;
+
+    printAll(names, points, n);
 
     delete[] names;
     delete[] points;
